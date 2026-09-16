2635c9816c1418e344a9aca9f850e6737cd2d3e2
Prabhuswar Saha <prabhuswarppsaha@gmail.com>
2026-09-16 11:23:04 +0530

    Update and rename pixelos.sh to derp.sh

--- Crave/derp.sh
+++ Crave/derp.sh
@@ -3,22 +3,27 @@
 rm -rf .repo/local_manifests/
 
 # Rom source repo
-repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr2 --git-lfs
+repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 17 --git-lfs --depth=1
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Clone local_manifests repository
-git clone -b lineage-16 https://github.com/prabhu992/local_manifests.git .repo/local_manifests
+git clone -b derp-17 https://github.com/prabhu992/local_manifests.git .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
+/opt/crave/resync.sh
+
 # Export
-export BUILD_USERNAME=mayuresh
+export BUILD_USERNAME=prabhu
 export BUILD_HOSTNAME=crave
 export TZ="Asia/India"
 echo "======= Export Done ======"
+
+rm -rf build/soong
+git clone https://github.com/yaap-17-stone/build_soong --depth=1 build/soong
 
 # Set up build environment
 . build/envsetup.sh
@@ -32,4 +37,4 @@ echo "============="
 m installclean
 
 # Build rom
-m pixelos
+m derp
