79ff43d5333811ce0a0a4360cfa78f9a76d0e923
Prabhuswar Saha <prabhuswarppsaha@gmail.com>
2026-09-18 20:13:31 +0530

    Update and rename circle.sh to evo.sh

--- Crave/evo.sh
+++ Crave/evo.sh
@@ -3,20 +3,18 @@
 rm -rf .repo/local_manifests/
 
 # Rom source repo
-repo init -u https://github.com/ProjectCiRCLE-ROM/manifest.git -b 16.2 --git-lfs --no-clone-bundle
+repo init -u https://github.com/Evolution-X/manifest.git -b cnb --git-lfs --depth=1
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Clone local_manifests repository
-git clone -b lineage-16 https://github.com/prabhu992/local_manifests.git .repo/local_manifests
+git clone -b derp-17 https://github.com/prabhu992/local_manifests.git .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
-# Sync the repositories
 /opt/crave/resync.sh
-echo "============================"
 
 # Export
 export BUILD_USERNAME=prabhu
@@ -24,10 +22,19 @@ export BUILD_HOSTNAME=crave
 export TZ="Asia/India"
 echo "======= Export Done ======"
 
+rm -rf build/soong
+git clone https://github.com/yaap-17-stone/build_soong --depth=1 build/soong
+
 # Set up build environment
 . build/envsetup.sh
 echo "====== Envsetup Done ======="
 
-# Brunch
-brunch stone
+# Lunch
+lunch lineage_stone-cp2a-user
 echo "============="
+
+# Install clean
+m installclean
+
+# Build rom
+m evolution
